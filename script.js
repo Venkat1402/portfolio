b2a7208304dd5fa27b780614e66219de6f464ba5
Venkat1402 <venkateshwarreddy140299@gmail.com>
2023-07-24 15:12:31 +0530

    Added new icons and its animations

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 // and adds and removes the class name .open when click event is detected
 
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+    // uncomment if need animation every time we scroll
+    // else {
+    //   entry.target.classList.remove("show");
+    // }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
